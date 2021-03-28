bbbde9ea1cb9f9a05fd3dc20defed125a333cb5f
swapnil358 <swapnil.bodade88@gmail.com>
2021-03-28 13:51:27 +0530

    first

---
+++ src/test/java/com/qa/test/BaseTest.java
@@ -0,0 +1,48 @@
+package com.qa.test;
+
+import java.util.Objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+	public static WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		if (driver==null) {
+
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.get("https://www.google.com");
+		}
+	}
+
+	@Test
+	public void searchGoogle1() {
+		driver.findElement(By.name("q")).sendKeys("selenium");
+	}
+
+	@Test
+	public void searchGoogle2() {
+		driver.findElement(By.name("q")).sendKeys("selenium");
+	}
+
+	@AfterMethod
+
+	public void tearDown() {
+
+		if (driver != null) {
+			driver.quit();
+			driver=null;
+		}
+	}
+
+}
